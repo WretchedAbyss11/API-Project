5d25ba50ab7e481cd8eeb38afbfbc7bb9eed7566
WretchedAbyss11 <d_walker98@hotmail.com>
2025-01-08 19:09:54 -0600

    API Learning Project completed

--- index.js
+++ index.js
@@ -3,19 +3,25 @@
 
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
-    const usersData = users.json();
-    
-    usersData.map((user) =>
-    const Data = `<div class="user-card">
+    const usersData = await users.json();
+    const userListEl = document.querySelector(".user-list")
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("")        
+}
+
+function showUserPosts(id) {
+  localStorage.setItem("id", id);
+  window.location.href = `${window.location.origin}/user.html`
+}
+
+function userHTML(user) {
+  return `<div class="user-card" onclick="showUserPosts(${user.id})">
             <div class="user-card__container">
-              <h3>User's Name</h4>
-                <p><b>Email:</b> email@email.com</p>
-                <p><b>Phone:</b> 0000000000</p>
-                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+              <h3>${user.name}</h4>
+                <p><b>Email:</b> ${user.email}</p>
+                <p><b>Phone:</b> ${user.phone}</p>
+                <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
             </div>
           </div>`
-            )
-            .join("")
 }
 
 main();
